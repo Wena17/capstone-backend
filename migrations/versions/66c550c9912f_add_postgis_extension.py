6a6c58a6f72a509bf8af22ca798f44c3cd6de477
Wena17 <pagayanan27@gmail.com>
2022-12-02 23:08:57 +0800

    Add postGIS extension

---
+++ migrations/versions/66c550c9912f_add_postgis_extension.py
@@ -0,0 +1,24 @@
+"""Add PostGIS extension
+
+Revision ID: 66c550c9912f
+Revises: 7f896a5b4f9a
+Create Date: 2022-12-02 23:03:54.385012
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '66c550c9912f'
+down_revision = '7f896a5b4f9a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("CREATE EXTENSION postgis")
+
+
+def downgrade():
+    pass
