d102ef54e0f0d63cfde3622fb95dffef0bd63e61
Wena17 <pagayanan27@gmail.com>
2023-01-10 16:27:12 +0800

    Edited get restoration details

--- app.py
+++ app.py
@@ -720,8 +720,8 @@ def ScheduleOutage():
 @app.route("/api/v1/restoration-details/<int:dev_id>", methods=['GET'])
 def restoration(dev_id):
     try:
-        query = select(Outage.outage_reason, Outage.est_end_time).filter_by(dev_id=dev_id)
-        exists = db.session.execute(query).first()
+        query = select(Outage.outage_reason, Outage.est_end_time).filter_by(dev_id=dev_id, end_time=None)
+        exists = db.session.execute(query).all()
         print("Exists: " + str(exists))
         # TODO return array of the response
         details = [{'reason': outage_reason, 'est_end_time': est_end_time}
